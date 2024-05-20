b675c727a21c265a6934d1d398bfc44858c9a762
OmarMWarraich <earthling986@gmail.com>
2024-05-21 04:41:13 +0500

    feat: JWT Revocation Strategy- JTIMatcher

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_20_231105) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_20_233817) do
   create_table "locations", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -34,7 +34,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_20_231105) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "jti", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
